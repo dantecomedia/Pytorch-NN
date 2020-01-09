951d4fb23f1016be62104f258197c6dc136d1651
RAHUL KUMAR <rahul.cse.ccu@gmail.com>
2020-01-09 16:50:36 +0530

    Create sequential.py
    Building sequential model using Pytorch

---
+++ sequential.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+
+input_size=784
+hidden_sizes=[128,64]
+output_size=10
+
+model=nn.Sequential(nn.Linear(input_size,hidden_sizes[0]),
+	nn.ReLU(),
+	nn.Linear(hidden_sizes[0],hidden_sizes[1]),
+	nn.ReLU(),
+	nn.Linear(hidden_sizes[1],output_size),
+	nn.Softmax(dim=1))
+
+print(model)
